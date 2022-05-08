28ae9f5b320282df4dc0e18cdc9e07677873afc5
Nikita Sidorov <nikas@leveltravels-MacBook-Pro.local>
2022-05-08 17:13:54 +0300

    add question validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
+
+  validates :title, :body, presence: true, length: { minimum: 5 }
 end
